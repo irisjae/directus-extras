7f34c1e15068a9324224033a4b3b5b684f8f79b6
jae <jae@jaes-MacBook-Air.local>
2026-05-04 15:42:36 +0800

    no email validation on login before auth

--- api/src/auth/drivers/local.ts
+++ api/src/auth/drivers/local.ts
@@ -51,7 +51,7 @@ export function createLocalAuthRouter(provider: string): Router {
 	const router = Router();
 
 	const userLoginSchema = Joi.object({
-		email: Joi.string().email().required(),
+		email: Joi.string().required(),
 		password: Joi.string().required(),
 		mode: Joi.string().valid('cookie', 'json', 'session'),
 		otp: Joi.string(),
